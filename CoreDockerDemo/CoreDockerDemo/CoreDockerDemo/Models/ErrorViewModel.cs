efe22de85410ec0de56ee8ef856d01bca1c5662f
MuralikrishnaKoneru <muralikrishnakoneruasus@gmail.com>
2024-03-11 14:59:10 +0530

    tesing

---
+++ CoreDockerDemo/CoreDockerDemo/CoreDockerDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreDockerDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
